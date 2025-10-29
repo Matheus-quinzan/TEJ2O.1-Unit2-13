da4e6563f3a65204e393bd002a4dd27c46132eeb
Matheus-quinzan <matheus.quinzan@stu.ocsb.ca>
2025-10-29 12:18:04 -0400

    a

--- main.ts
+++ main.ts
@@ -5,4 +5,18 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variable
+let counter : number = 4
+let neopixels
+
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Duck)
+
+input.onButtonPressed(Button.A, function() {
+    while (counter > 0){
+        basic.showNumber(counter)
+        neopixel.create(DigitalPin.P0, 16, NeoPixelMode.RGB)
+        
+    }
+})
