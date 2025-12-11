0532f80945b74b1241d45a0953d34fe43fe1246f
Matheus-quinzan <matheus.quinzan@stu.ocsb.ca>
2025-12-11 17:39:39 -0500

    turns on neopixels based on the number displayed on the micro bit

--- main.ts
+++ main.ts
@@ -1,22 +1,80 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matheus Quinzan
+ * Created on: Oct 2025
+ * This program turns on neopixels based on the number displayed on the micro bit
 */
 
-// variable
-let counter : number = 4
-let neopixels
+// variables
+let loopCounter: number = 4
+let neopixelStrip: neopixel.Strip = null
 
-//setup
+// setup
 basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
 basic.showIcon(IconNames.Duck)
 
-input.onButtonPressed(Button.A, function() {
-    while (counter > 0){
-        basic.showNumber(counter)
-        neopixel.create(DigitalPin.P0, 16, NeoPixelMode.RGB)
-        
+// turn on all the lights
+input.onButtonPressed(Button.A, function () {
+    while (loopCounter > 0) {
+
+        if (loopCounter == 4) {
+            // lights up 4 neopixels
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.show()
+            basic.showNumber(loopCounter)
+            loopCounter = loopCounter - 1
+        }
+
+        if (loopCounter == 3) {
+            // lights up 3 neopixels
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.showNumber(loopCounter)
+            loopCounter = loopCounter - 1
+        }
+
+        if (loopCounter == 2) {
+            // lights up 2 neopixels
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.showNumber(loopCounter)
+            loopCounter = loopCounter - 1
+        }
+
+        if (loopCounter == 1) {
+            // lights up 1 neopixel
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.showNumber(loopCounter)
+            loopCounter = loopCounter - 1
+        }
+
+    } if (loopCounter == 0) {
+        // turns off all neopixels on robot bit
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.showNumber(loopCounter)
     }
+    basic.showIcon(IconNames.Duck)
 })
